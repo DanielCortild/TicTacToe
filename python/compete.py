9cdbcaa0b8a63c7b0f7babfd2d5c11367907f73b
DanielCortild <daniel@cortild.dk>
2020-01-16 21:58:55 +0100

    Added Raw Python Files

---
+++ python/compete.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: daniel
+
+Functions
+"""
+from modules.players import AIPlayer, RandomPlayer, HumanPlayer, DLPlayer
+from modules.functions import compete
+
+import sys
+
+print( "Choose Players from: Rand, Q, DL" )
+P1_Label = input( "P1: " )
+P2_Label = input( "P2: " )
+
+if P1_Label == 'Rand':
+    P1 = RandomPlayer()
+elif P1_Label == 'Q':
+    P1 = AIPlayer()
+elif P1_Label == 'DL':
+    P1 = DLPlayer()
+else:
+    print( "Choice for P1 not allowed" )
+    sys.exit()
+
+if P2_Label == 'Rand':
+    P2 = RandomPlayer()
+elif P2_Label == 'Q':
+    P2 = AIPlayer()
+elif P2_Label == 'DL':
+    P2 = DLPlayer()
+else:
+    print( "Choice for P2 not allowed" )
+    sys.exit()
+
+ep = int( input( "Number of Epochs: " ) )
+
+compete( P1, P2, epochs=ep )
---
+++ python/train.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: daniel
+
+Functions
+"""
+from modules.players import AIPlayer, RandomPlayer, HumanPlayer, DLPlayer
+from modules.functions import train
+
+print( "Choose Players from: Q, DL" )
+P1_Label = input( "P1: " )
+P2_Label = input( "P2: " )
+
+if P1_Label == 'Q':
+    P1 = AIPlayer(exp=0.1)
+elif P1_Label == 'DL':
+    P1 = DLPlayer(exp=0.1)
+else:
+    print( "Choice for P1 not allowed" )
+    sys.exit()
+
+if P2_Label == 'Q':
+    P2 = AIPlayer(exp=0.1)
+elif P2_Label == 'DL':
+    P2 = DLPlayer(exp=0.1)
+else:
+    print( "Choice for P2 not allowed" )
+    sys.exit()
+
+ep = int( input( "Number of Epochs: " ) )
+
+train( P1, P2, epochs=ep )
